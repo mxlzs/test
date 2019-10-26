094ce90f68f35cf1b70d632ee8b9808af475e166
whm <1183561690@qq.com>
2019-10-26 16:09:07 +0800

    王海盟第二次提交(客户 验证 搜索)

--- app/Http/Controllers/admin/Client.php
+++ app/Http/Controllers/admin/Client.php
@@ -16,6 +16,31 @@ class Client extends Controller
 
     //客户添加执行
     function create_do(){
+        $validatedData = request()->validate([
+            'admin_name'   => 'required|min:2|max:8|alpha_dash',
+            'client_name'  => 'required|min:2|max:8|alpha_dash',
+            'client_short' => 'required|min:2|max:6|alpha_dash',
+            'client_coding'=> 'required|numeric',
+            'tel'          => 'digits:11',
+            'email'        => 'email'
+        ],[
+            'admin_name.required'=>'销售员不能为空',
+            'admin_name.min'=>'销售员格式为:2~8位中文,数字,字母,下划线',
+            'admin_name.max'=>'销售员格式为:2~8位中文,数字,字母,下划线',
+            'admin_name.alpha_dash'=>'销售员格式为:2~8位中文,数字,字母,下划线',
+            'client_name.required'=>'客户不能为空',
+            'client_name.min'=>'客户格式为:2~8位中文,数字,字母,下划线',
+            'client_name.max'=>'客户格式为:2~8位中文,数字,字母,下划线',
+            'client_name.alpha_dash'=>'客户格式为:2~8位中文,数字,字母,下划线',
+            'client_short.required'=>'客户简称不能为空',
+            'client_short.min'=>'客户简称格式为:2~6位中文,数字,字母,下划线',
+            'client_short.max'=>'客户简称格式为:2~6位中文,数字,字母,下划线',
+            'client_short.alpha_dash'=>'客户简称格式为:2~6位中文,数字,字母,下划线',
+            'client_coding.required'=>'客户编号不能为空',
+            'client_coding.numeric'=>'客户编号必须为数字',
+            'email.email'=>'邮箱格式不正确',
+            'tel.digits'=>'手机号不正确'
+        ]);
         $data=request()->except('_token');
         $res=client2::create($data);
         if($res){
@@ -27,8 +52,14 @@ class Client extends Controller
 
     //列表
     function index(){
-        $client_info=client2::paginate(2);
-        return view('client/index',['client_info'=>$client_info]);
+        $client_coding=request()->client_coding;
+        $where=[];
+        if($client_coding){
+            $where['client_coding']=['client_coding'=>$client_coding];
+        }
+        $client_info=client2::where($where)->paginate(2);
+        $query=request()->all();
+        return view('client/index',['client_info'=>$client_info,'query'=>$query]);
     }
 
     function del($client_id){
